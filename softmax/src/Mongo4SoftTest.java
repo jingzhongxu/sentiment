61af39e7b47f4287d7b7d757931248575739448c
Ericva <super.magicli@gmail.com>
2014-05-09 00:23:45 +0800

    commit

---
+++ softmax/src/Mongo4SoftTest.java
@@ -0,0 +1,22 @@
+// 这是我用来测试用java处理softmax的训练数据的类
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.WriteConcern;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.ServerAddress;
+
+
+
+
+public class Mongo4SoftTest
+{
+	public int parameterNums;
+	
+
+
+}
---
+++ softmax/src/SoftmaxRegressionWithMongo.java
@@ -0,0 +1,15 @@
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.WriteConcern;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.ServerAddress;
+
+
+public class SoftmaxRegressionWithMongo
+{
+
+}
